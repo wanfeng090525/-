168b896e9d02446a1b7117b2e154788d338c1584
VLC-iOS WHO <dev@vlc.local>
2026-08-29 03:19:54 +0000

    fix(m3u): bulletproof M3U fragment (drop ActivityResultContracts, safe card bg attr)

--- application/vlc-android/src/org/videolan/vlc/gui/M3UPlaylistFragment.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/M3UPlaylistFragment.kt
@@ -4,6 +4,7 @@
  */
 package org.videolan.vlc.gui
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.InputType
@@ -11,7 +12,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -25,19 +25,27 @@ import org.videolan.vlc.util.FileUtils
 
 class M3UPlaylistFragment : Fragment() {
 
-    private val pickM3U = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
-        if (uri != null) playSource(uri)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.m3u_playlist_fragment, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<View>(R.id.local_file_card).setOnClickListener {
-            pickM3U.launch(arrayOf("*/*"))
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "*/*"
+            }
+            startActivityForResult(intent, REQUEST_PICK_M3U)
         }
         view.findViewById<View>(R.id.url_card).setOnClickListener { showUrlDialog() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_PICK_M3U && resultCode == android.app.Activity.RESULT_OK && data?.data != null) {
+            playSource(data.data!!)
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     private fun showUrlDialog() {
@@ -61,5 +69,9 @@ class M3UPlaylistFragment : Fragment() {
             val media = MLServiceLocator.getAbstractMediaWrapper(resolved)
             MediaUtils.openMedia(requireContext(), media)
         }
+    }
+
+    companion object {
+        private const val REQUEST_PICK_M3U = 0xC0FFEE
     }
 }
